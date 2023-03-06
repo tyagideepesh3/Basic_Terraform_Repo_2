7ec1bbdf137e5c2eb3942ecdcb4e0a6662af35e9
tyagideepesh3 <64723395+tyagideepesh3@users.noreply.github.com>
2023-03-06 17:24:49 +0530

    DynamoDb_resource_commit

--- main.tf
+++ main.tf
@@ -32,21 +32,81 @@ resource "aws_s3_bucket_versioning" "blog-bucket-versioning" {
     status = "Enabled"
   }
 }
-module "first-dynamodb-table-tf-module" {
-  source  = "terraform-aws-modules/dynamodb-table/aws"
-  version = "3.1.2"
-
-  name     = "My_First_Table"
-  hash_key = "UserId"
-  
-  attributes = [
-    {
-        name = "UserId",
-        type = "S"
-    }
-  ]
-  tags = {
-    Terraform = "true"
-    Environment = "staging"
+#module "first-dynamodb-table-tf-module" {
+#  source  = "terraform-aws-modules/dynamodb-table/aws"
+#  version = "3.1.2"
+#
+#  name     = "My_First_Table"
+#  hash_key = "UserId"
+#  
+# attributes = [
+#    {
+#        name = "UserId",
+#        type = "S"
+#    }
+#  ]
+#  tags = {
+#    Terraform = "true"
+#    Environment = "staging"
+#  }
+#}
+resource "aws_dynamodb_table" "res-dynamodb-table-tf-rec" {
+  name = "User-Table"
+  billing_mode = "PROVISIONED"
+  read_capacity = 5
+  write_capacity = 5
+  attribute {
+    name = "UserId"
+    type = "S"
   }
+  hash_key = "UserId"
+  tags = {
+    name        = "Demo Dynamo Db"
+    Environment = "testing"
+  }
+}
+resource "aws_dynamodb_table_item" "item1" {
+  depends_on = [
+    aws_dynamodb_table.res-dynamodb-table-tf-rec
+  ]
+  table_name = aws_dynamodb_table.res-dynamodb-table-tf-rec
+  hash_key   = aws_dynamodb_table.res-dynamodb-table-tf-rec.hash_key
+  item = <<ITEM
+  {
+    "UserId" : {"S":"001"},
+    "UserName" : {"S":"Devu"},
+    "Phone Number" : {"N":9272111100},
+    "UserAddress" : {"S": "874/2-1 , dev prayag , UTTARAKHAND"}
+  }
+  ITEM
+}
+resource "aws_dynamodb_table_item" "item2" {
+  depends_on = [
+    aws_dynamodb_table.res-dynamodb-table-tf-rec
+  ]
+  table_name = aws_dynamodb_table.res-dynamodb-table-tf-rec
+  hash_key   = aws_dynamodb_table.res-dynamodb-table-tf-rec.hash_key
+  item = <<ITEM
+  {
+    "UserId" : {"S":"002"},
+    "UserName" : {"S":"Deepu"},
+    "Phone Number" : {"N":9272111101},
+    "UserAddress" : {"S": "874/2-2 , dev prayag , UTTARAKHAND"}
+  }
+  ITEM
+}
+resource "aws_dynamodb_table_item" "item3" {
+  depends_on = [
+    aws_dynamodb_table.res-dynamodb-table-tf-rec
+  ]
+  table_name = aws_dynamodb_table.res-dynamodb-table-tf-rec
+  hash_key   = aws_dynamodb_table.res-dynamodb-table-tf-rec.hash_key
+  item = <<ITEM
+  {
+    "UserId" : {"S":"001"},
+    "UserName" : {"S":"Ayush"},
+    "Phone Number" : {"N":9272111102},
+    "UserAddress" : {"S": "874/3-3 , dev prayag , UTTARAKHAND"}
+  }
+  ITEM
 }
--- outputs.tf
+++ outputs.tf
@@ -7,6 +7,6 @@ output "aws_bucket_arn" {
 output "aws_bucket_region" {
   value = aws_s3_bucket.blog-bucket.region
 }
-output "aws_dynamoDb_Id" {
-  value = module.first-dynamodb-table-tf-module.dynamodb_table_id
+output "aws_Dynamo_Db_Id" {
+  value = aws_dynamodb_table.res-dynamodb-table-tf-rec.id
 }
